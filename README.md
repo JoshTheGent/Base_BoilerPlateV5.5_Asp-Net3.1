be4220e9562e7e01d74bec8ed49d6912fa882f25
Josh <j0sh7ran@gmail.com>
2020-05-29 20:46:17 -0500

    updating login page

--- README.md
+++
@@ -1,2 +0,0 @@
-# Base_BoilerPlateV5.5_Asp-Net3.1
-Updated Version of BoilerPlate v 5.5 that has an updated template style from https://templated.co/ using template: 'templated-industrious'
--- src/JoshTheGent.Web.Mvc/wwwroot/view-resources/Views/Account/Login.js
+++ src/JoshTheGent.Web.Mvc/wwwroot/view-resources/Views/Account/Login.js
@@ -1,6 +1,17 @@
 ﻿(function () {
     $('#ReturnUrlHash').val(location.hash);
 
+
+    function onSignIn(googleUser) {
+        debugger;
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+
+
     var _$form = $('#LoginForm');
 
     _$form.submit(function (e) {
@@ -12,11 +23,13 @@
 
         abp.ui.setBusy(
             $('body'),
-
             abp.ajax({
                 contentType: 'application/x-www-form-urlencoded',
                 url: _$form.attr('action'),
                 data: _$form.serialize()
+            }).done(function (data) {
+                debugger;
+                console.log(data);
             })
         );
     });
